7ff1108debc46f15a206d53cad963ae6994072f0
Vincent Lanier <vlanier@uoregon.edu>
2023-03-04 16:49:15 -0800

    factored out with checkpoint list helper func

--- api/resources/brevet.py
+++ api/resources/brevet.py
@@ -3,11 +3,10 @@ Resource: Brevet
 """
 from flask import Response, request
 from flask_restful import Resource
-from datetime import datetime
 import logging
+from datetime import datetime
 
-# You need to implement this in database/models/
-from database.models import Brevet, Checkpoint
+from database.models import Brevet, Checkpoint, build_checkpoint_list
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -18,48 +17,14 @@ class BrevetApi(Resource):
     
     def put(self, _id):
         body = request.get_json()
-
-        logging.debug(body)
-
-        checkpoints = list()
-        for checkpoint_key in body['checkpoints']:
-
-
-
-            data = body['checkpoints'][checkpoint_key]
-            checkpoints.append(
-                Checkpoint(data['distance'], data['location'],
-                           datetime.fromisoformat(data['open_time']),
-                           datetime.fromisoformat(data['close_time']))
-            )
-        brevet = Brevet.objects.get(id=_id) #.update(**body)
-        brevet.update(length = body["length"])
-        brevet.update(checkpoints = checkpoints)
-        brevet.update(start_time = datetime.fromisoformat(body["start_time"]))
+        checkpoints = build_checkpoint_list(body["checkpoints"])
+        brevet = Brevet.objects.get(id=_id)
+        brevet.update(length = body["length"], 
+                      checkpoints = checkpoints,
+                      start_time = datetime.fromisoformat(body["start_time"]))
 
         return {'id' : str(_id), 'status' : 'updated'}, 200
     
     def delete(self, _id):
         Brevet.objects.get(id=_id).delete()
         return {'id' : str(_id), 'status' : 'deleted'}, 200
-
-# MongoEngine queries:
-# Brevet.objects() : similar to find_all. Returns a MongoEngine query
-# Brevet(...).save() : creates new brevet
-# Brevet.objects.get(id=...) : similar to find_one
-
-# Two options when returning responses:
-#
-# return Response(json_object, mimetype="application/json", status=200)
-# return python_dict, 200
-#
-# Why would you need both?
-# Flask-RESTful's default behavior:
-# Return python dictionary and status code,
-# it will serialize the dictionary as a JSON.
-#
-# MongoEngine's objects() has a .to_json() but not a .to_dict(),
-# So when you're returning a brevet / brevets, you need to convert
-# it from a MongoEngine query object to a JSON and send back the JSON
-# directly instead of letting Flask-RESTful attempt to convert it to a
-# JSON for you.
--- api/resources/brevets.py
+++ api/resources/brevets.py
@@ -6,8 +6,7 @@ from flask_restful import Resource
 import logging
 from datetime import datetime
 
-# You need to implement this in database/models/
-from database.models import Brevet, Checkpoint
+from database.models import Brevet, Checkpoint, build_checkpoint_list
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -17,29 +16,8 @@ class BrevetsApi(Resource):
         return Response(brevets, mimetype="application/json", status=200)
 
     def post(self):
-
-        logging.debug("POST")
-
         body = request.get_json()
-
-        logging.debug(body)
-        logging.debug(body['checkpoints'])
-
-        checkpoints = list()
-        for data in body['checkpoints']:
-            
-            checkpoints.append(
-                Checkpoint(distance=data['distance'], location=data['location'],
-                           open_time=datetime.fromisoformat(data['open_time']),
-                           close_time=datetime.fromisoformat(data['close_time']))
-            )
-
-        logging.debug("SAVE")
-
-        for obj in body:
-            logging.debug(obj)
-            logging.debug(body[obj])
-
+        checkpoints = build_checkpoint_list(body['checkpoints'])
         brevet = Brevet()
         brevet.length = body['length']
         brevet.start_time = datetime.fromisoformat(body['start_time'])
@@ -47,24 +25,3 @@ class BrevetsApi(Resource):
         brevet.save()
         id = brevet.id
         return {'id' : str(id)}, 200
-
-# MongoEngine queries:
-# Brevet.objects() : similar to find_all. Returns a MongoEngine query
-# Brevet(...).save() : creates new brevet
-# Brevet.objects.get(id=...) : similar to find_one
-
-# Two options when returning responses:
-#
-# return Response(json_object, mimetype="application/json", status=200)
-# return python_dict, 200
-#
-# Why would you need both?
-# Flask-RESTful's default behavior:
-# Return python dictionary and status code,
-# it will serialize the dictionary as a JSON.
-#
-# MongoEngine's objects() has a .to_json() but not a .to_dict(),
-# So when you're returning a brevet / brevets, you need to convert
-# it from a MongoEngine query object to a JSON and send back the JSON
-# directly instead of letting Flask-RESTful attempt to convert it to a
-# JSON for you.
