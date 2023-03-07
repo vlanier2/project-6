bc7b52b2ca56a594be52f9b7a1a261512b0ca28a
Vincent Lanier <vlanier@uoregon.edu>
2023-03-07 12:47:17 -0800

    fixed bug with popup on api delete

--- brevets/brevets_db.py
+++ brevets/brevets_db.py
@@ -13,16 +13,17 @@ def APIinsert(data_dict, api_url):
 def APIdisplay(api_url):
     
     result = requests.get(api_url)
+    brevets = result.json()
 
-    if result.status_code != 200:
+
+    if result.status_code != 200 or len(brevets) == 0:
         return {}
     
-    last_brevet_saved = result.json()[-1]
+    last_brevet_saved = brevets[-1]
     last_brevet_saved.pop('_id')
     last_brevet_saved['start_time'] = datetime.utcfromtimestamp(last_brevet_saved['start_time']['$date'] // 1000).isoformat()
     for checkpoint in last_brevet_saved['checkpoints']:
         checkpoint['open_time'] = datetime.utcfromtimestamp(checkpoint['open_time']['$date'] // 1000).isoformat()
         checkpoint['close_time'] = datetime.utcfromtimestamp(checkpoint['close_time']['$date'] // 1000).isoformat()
-        checkpoint.pop('_cls')
 
     return last_brevet_saved
