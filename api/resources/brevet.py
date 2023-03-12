a06ac061c38dd43b6178b8be98cc3743c8a7cadf
Vincent Lanier <vlanier@uoregon.edu>
2023-03-12 14:11:22 -0700

    fixed error with put requring all fields

--- api/resources/brevet.py
+++ api/resources/brevet.py
@@ -16,12 +16,22 @@ class BrevetApi(Resource):
         return Response(brevet, mimetype="application/json", status=200)
     
     def put(self, _id):
+
         body = request.get_json()
-        checkpoints = build_checkpoint_list(body["checkpoints"])
         brevet = Brevet.objects.get(id=_id)
-        brevet.update(length = body["length"], 
-                      checkpoints = checkpoints,
-                      start_time = datetime.fromisoformat(body["start_time"]))
+
+        update_query = {}
+
+        if "length" in body:
+            update_query["length"] = body["length"]
+
+        if "checkpoints" in body:
+            update_query["checkpoints"] = build_checkpoint_list(body["checkpoints"])
+
+        if "start_time" in body:
+            update_query["start_time"] = datetime.fromisoformat(body["start_time"])
+
+        brevet.update(**update_query)
 
         return {'id' : str(_id), 'status' : 'updated'}, 200
     
